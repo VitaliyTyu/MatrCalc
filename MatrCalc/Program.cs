25db2fda61f975456247cfaff38bb5f6ad631e2e
VitaliyTyu <VitaliyTyu@yandex.ru>
2022-09-11 20:36:03 +0500

    Add project files.

---
+++ MatrCalc/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrCalc
+{
+    class Program
+    {
+        public static Random Rnd { get; set; } = new Random();
+
+        static void Main()
+        {
+            double[,] matrix = CreateMatrix();
+
+            var det = DetRec(matrix);
+
+            Print(matrix);
+            Console.WriteLine($"\nДетерминант: {det}");
+        }
+
+        private static double[,] CreateMatrix()
+        {
+            Console.Write("Введите размер квадратной матрицы: ");
+            int n = int.Parse(Console.ReadLine());
+            //int n = int.MaxValue;
+            var matrix = new double[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = Rnd.Next(0, 100);
+                }
+            }
+            return matrix;
+        }
+
+        private static void Print(double[,] matrix)
+        {
+            Console.WriteLine("Матрица:");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static double DetRec(double[,] matrix)
+        {
+            if (matrix.Length == 4)
+            {
+                return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+            }
+            double sign = 1, result = 0;
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                double[,] minor = GetMinor(matrix, i);
+                result += sign * matrix[0, i] * DetRec(minor);
+                sign = -sign;
+            }
+            return result;
+        }
+
+        private static double[,] GetMinor(double[,] matrix, int n)
+        {
+            double[,] result = new double[matrix.GetLength(0) - 1, matrix.GetLength(0) - 1];
+            for (int i = 1; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0, col = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (j == n)
+                        continue;
+                    result[i - 1, col] = matrix[i, j];
+                    col++;
+                }
+            }
+            return result;
+        }
+    }
+}
